2bac985a4a4ed1a0d98edac72fba11b7bc7629bf
Zhenya <fedyai.eugen@gmail.com>
2018-03-29 19:02:43 +0300

    Add avatar module

--- js/drag.js
+++ js/drag.js
@@ -1,7 +1,8 @@
 'use strict';
 
 (function () {
-  var windowHandle = window.dialog.setupWindow.querySelector('.setup-user-pic');
+  var windowHandler = window.dialog.setupWindow.querySelector('.setup-user-pic');
+
   var windowSetupInitialPosition = {
     x: window.dialog.setupWindow.style.left,
     y: window.dialog.setupWindow.style.top
@@ -10,22 +11,30 @@
   var onMouseDownClick = function (event) {
     event.preventDefault();
 
+    var dragged = false;
     var startSetupCordinate = {
       x: window.dialog.setupWindow.offsetLeft,
       y: window.dialog.setupWindow.offsetTop
     };
+
     var innerShift = {
       x: startSetupCordinate.x - event.clientX,
       y: startSetupCordinate.y - event.clientY
     };
+
     var onMouseMove = function (eventMouseMove) {
       eventMouseMove.preventDefault();
+      dragged = true;
 
       window.dialog.setupWindow.style.top = (eventMouseMove.clientY + innerShift.y) + 'px';
       window.dialog.setupWindow.style.left = (eventMouseMove.clientX + innerShift.x) + 'px';
     };
+
     var onMouseUp = function (eventMouseUp) {
       eventMouseUp.preventDefault();
+      if (dragged) {
+
+      }
 
       document.removeEventListener('mousemove', onMouseMove);
       document.removeEventListener('mouseup', onMouseUp);
@@ -35,7 +44,7 @@
     document.addEventListener('mouseup', onMouseUp);
   };
 
-  windowHandle.addEventListener('mousedown', onMouseDownClick);
+  windowHandler.addEventListener('mousedown', onMouseDownClick);
 
   window.drag = {
     setUserSetupInitialPosition: function () {
