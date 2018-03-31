c4b608ccb73940db9d3cbec4339f0d93ac2e20c5
Zhenya <fedyai.eugen@gmail.com>
2018-03-31 11:35:24 +0300

    Resolve conflict with image and input file click handler

--- js/drag.js
+++ js/drag.js
@@ -1,7 +1,8 @@
 'use strict';
 
 (function () {
-  var windowHandler = window.dialog.setupWindow.querySelector('.setup-user-pic');
+  var windowHandler = window.dialog.setupWindow.querySelector('.upload');
+  var dragged;
 
   var windowSetupInitialPosition = {
     x: window.dialog.setupWindow.style.left,
@@ -11,7 +12,7 @@
   var onMouseDownClick = function (event) {
     event.preventDefault();
 
-    var dragged = false;
+    dragged = false;
     var startSetupCordinate = {
       x: window.dialog.setupWindow.offsetLeft,
       y: window.dialog.setupWindow.offsetTop
@@ -22,18 +23,23 @@
       y: startSetupCordinate.y - event.clientY
     };
 
-    var onMouseMove = function (eventMouseMove) {
-      eventMouseMove.preventDefault();
+    var onMouseMove = function (moveEvent) {
+      moveEvent.preventDefault();
       dragged = true;
 
-      window.dialog.setupWindow.style.top = (eventMouseMove.clientY + innerShift.y) + 'px';
-      window.dialog.setupWindow.style.left = (eventMouseMove.clientX + innerShift.x) + 'px';
+      window.dialog.setupWindow.style.top = (moveEvent.clientY + innerShift.y) + 'px';
+      window.dialog.setupWindow.style.left = (moveEvent.clientX + innerShift.x) + 'px';
     };
 
-    var onMouseUp = function (eventMouseUp) {
-      eventMouseUp.preventDefault();
-      if (dragged) {
+    var onMouseUp = function (upEvent) {
+      upEvent.preventDefault();
 
+      if (dragged) {
+        var onClickPreventDefault = function (clickEvent) {
+          clickEvent.preventDefault();
+          windowHandler.removeEventListener('click', onClickPreventDefault);
+        };
+        windowHandler.addEventListener('click', onClickPreventDefault);
       }
 
       document.removeEventListener('mousemove', onMouseMove);
